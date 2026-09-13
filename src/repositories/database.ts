803b825c5cc3b82ea58a0fa5e11038cfeadc67a1
luinbiotech-lab <luinbiotech@gmail.com>
2026-09-13 21:40:52 +0900

    feat: add risk assessment store

--- src/repositories/database.ts
+++ src/repositories/database.ts
@@ -1,14 +1,14 @@
 import { openDB } from 'idb';
 
 export const DATABASE_NAME = 'real-estate-report';
-export const DATABASE_VERSION = 7;
+export const DATABASE_VERSION = 8;
 
 export const database = openDB(DATABASE_NAME, DATABASE_VERSION, {
   upgrade(db) {
     if (!db.objectStoreNames.contains('properties')) db.createObjectStore('properties', { keyPath: 'id' });
     if (!db.objectStoreNames.contains('settings')) db.createObjectStore('settings');
     if (!db.objectStoreNames.contains('importJobs')) db.createObjectStore('importJobs', { keyPath: 'id' });
-    for (const name of ['propertyDocuments', 'propertyMedia', 'propertyVerifications', 'propertyVerificationCandidates', 'propertyDataSources', 'reportSnapshots', 'digitalTwinAssets', 'agentJobs', 'agentResults', 'agentReviews', 'propertySpaces', 'spaceMediaLinks', 'propertyFacilities', 'renovationAssessments']) {
+    for (const name of ['propertyDocuments', 'propertyMedia', 'propertyVerifications', 'propertyVerificationCandidates', 'propertyDataSources', 'reportSnapshots', 'digitalTwinAssets', 'agentJobs', 'agentResults', 'agentReviews', 'propertySpaces', 'spaceMediaLinks', 'propertyFacilities', 'renovationAssessments', 'riskAssessments']) {
       if (!db.objectStoreNames.contains(name)) {
         const store = db.createObjectStore(name, { keyPath: 'id' });
         store.createIndex('propertyId', 'propertyId');
