cf0f40816f991331426d9681fddb4ccc81175bb9
luinbiotech-lab <luinbiotech@gmail.com>
2026-09-22 20:21:08 +0900

    feat: make approved DAON pages the default report flow

--- src/components/professionalReport/DaonProfessionalReportMaster.tsx
+++ src/components/professionalReport/DaonProfessionalReportMaster.tsx
@@ -1,9 +1,13 @@
 import type { ReportSnapshot } from '../../domain/propertyDataRoom/types';
 import type { ProfessionalReportViewModel } from '../../domain/professionalReport/types';
 import { DAON_DETAIL_MASTER_TEMPLATE_ID, DAON_DETAIL_MASTER_TEMPLATE_VERSION } from '../../domain/professionalReport/templateIds';
-import { DaonDetail7PageMaster } from './DaonDetail7PageMaster';
 import { DaonReportOpeningPage } from './DaonReportOpeningPage';
 import { DaonReportClosingPage } from './DaonReportClosingPage';
+import {
+  DaonPropertySummaryMasterPage,
+  DaonInvestmentAnalysisMasterPage,
+  DaonDevelopmentDeepDiveMasterPage,
+} from './DaonApprovedCorePages';
 
 export function DaonProfessionalReportMaster({ snapshot, model }: { snapshot: ReportSnapshot; model: ProfessionalReportViewModel }) {
   return <div
@@ -11,9 +15,12 @@ export function DaonProfessionalReportMaster({ snapshot, model }: { snapshot: Re
     data-template-id={DAON_DETAIL_MASTER_TEMPLATE_ID}
     data-template-version={DAON_DETAIL_MASTER_TEMPLATE_VERSION}
     data-visual-master="DAON_VISUAL_MASTER_2026_09_22"
+    data-snapshot-version={snapshot.reportVersion}
   >
     <DaonReportOpeningPage model={model} />
-    <DaonDetail7PageMaster snapshot={snapshot} model={model} />
+    <DaonPropertySummaryMasterPage model={model} />
+    <DaonInvestmentAnalysisMasterPage model={model} />
+    <DaonDevelopmentDeepDiveMasterPage model={model} />
     <DaonReportClosingPage model={model} />
   </div>;
 }
